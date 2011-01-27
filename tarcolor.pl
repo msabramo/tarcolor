4cfb0827da76412422983ee1a20fb749ffd3adde
Marc Abramowitz <marc@marc-abramowitz.com>
2011-01-27 10:02:13 -0800

    Add comment explaining what program does and give analogous LS_COLORS (GNU ls) setting.

--- tarcolor.pl
+++ tarcolor.pl
@@ -2,6 +2,12 @@
 
 use strict;
 
+# Colors output of `tar tvxf` or `ls -l` the way GNU ls (in GNU coreutils)
+# would color a directory listing if the environment variable LS_COLORS was set
+# to:
+#
+# LS_COLORS='di=01;34:ln=01;36:ex=01;32:
+
 my $CYAN     = "\033[1;36m";
 my $BLUE     = "\033[1;34m";
 my $GREEN    = "\033[1;32m";
