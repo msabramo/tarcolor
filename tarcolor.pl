9dbb8cf5ced4954638addb1e7c7ac635fcfcf10d
Marc Abramowitz <marc@marc-abramowitz.com>
2011-01-27 08:58:48 -0800

    Use warnings and strict and create variables with ANSI color escape values.

--- tarcolor.pl
+++ tarcolor.pl
@@ -1,17 +1,24 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+
+use strict;
+
+my $CYAN     = "\033[1;36m";
+my $BLUE     = "\033[1;34m";
+my $GREEN    = "\033[1;32m";
+my $RESET    = "\033[0m";
 
 while (<>) {
-    $line = $_;
-    $filename = (split())[8];
+    my $line = $_;
+    my $filename = (split())[8];
     
     if (substr($line, 0, 1) eq 'l') {
-        s#\Q$filename\E#\033[1;36m$filename\033[0m#;
+        s {$filename} {${CYAN}${filename}${RESET}}x;
     }
     elsif (substr($line, 0, 1) eq 'd') {
-        s#\Q$filename\E#\033[1;34m$filename\033[0m#;
+        s {$filename} {${BLUE}${filename}${RESET}}x;
     }
     elsif (substr($line, 3, 1) eq 'x') {
-        s#\Q$filename\E#\033[1;32m$filename\033[0m#;
+        s {$filename} {${GREEN}${filename}${RESET}}x;
     }
 
     print;
