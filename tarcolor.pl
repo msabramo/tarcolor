057310d43f06f02de8e15cf36beabac0aed1b50d
Marc Abramowitz <marc@marc-abramowitz.com>
2012-01-16 06:22:24 -0800

    Use regexes exclusively for determining filename and coloring; we don't try to read the input as columns; this can get hairy when group names and filenames have spaces in them.

--- tarcolor.pl
+++ tarcolor.pl
@@ -1,35 +1,26 @@
 #!/usr/bin/perl -w
 
-use strict;
-use Getopt::Long;
-use Scalar::Util qw(looks_like_number);
-
+# tarcolor
+#
+# by Marc Abramowitz <marc at marc-abramowitz dot com>
+#
+# https://github.com/msabramo/tarcolor
+#
 # Colors output of `tar tvf` or `ls -l` similarly to the way GNU ls (in GNU
 # coreutils) would color a directory listing.
 #
 # Colors can be customized using an environment variable:
 #
-# TAR_COLORS='di=01;34:ln=01;36:ex=01;32:so=01;40:pi=01;40:bd=34;46:cd=34;43:su=0;41:sg=0;46'
+# TAR_COLORS='di=01;34:ln=01;36:ex=01;32:so=01;40:pi=01;40:bd=40;33:cd=40;33:su=0;41:sg=0;46'
 #
 # The format for TAR_COLORS is similar to the format used by LS_COLORS
 # Check out the online LSCOLORS generator at http://geoff.greer.fm/lscolors/
 
-my %FILE_TYPE_TO_COLOR = (
-	"di" => "\033[01;34m",
-	"ln" => "\033[01;36m",
-	"ex" => "\033[01;32m",
-	"so" => "\033[01;35m",
-	"pi" => "\033[01;40m",
-	"bd" => "\033[34;46m",
-	"cd" => "\033[34;43m",
-);
-
-foreach (split(':', $ENV{'TAR_COLORS'})) {
-	my ($type, $codes) = split('=');
-	$FILE_TYPE_TO_COLOR{$type} = "\033[" . $codes . "m";
-}
+use warnings;
+use strict;
 
 my $RESET = "\033[0m";
+
 
 sub get_file_type {
     if (substr($_, 0, 1) eq 'l') {
@@ -56,35 +47,44 @@ sub get_file_type {
 }
 
 sub color_filename {
-    my ($filename, $color) = @_;
+    my ($color) = @_;
 
-    s {$filename} {${color}${filename}${RESET}}x;
+    s
+	    {
+			(
+				(?:\s+\d{2}:\d{2}) |     # time
+		        (?:\s+\d{4})             # year
+		    )                            # $1 = time | year
+			(\s+)                        # $2 = space
+			(.+?)                        # $3 = filename
+			(\s->|$)                     # $4 = " ->" or end of string
+		}
+		{$1$2${color}$3${RESET}$4}x;
 }
 
-sub get_filename_column {
-	my (@fields) = @_;
 
-	if (looks_like_number($fields[1])) {
-		return 8;
-	} else {
-		return 5;
-	}
-}
-
-my $filename_column = 8;
-
-my $result = GetOptions(
-    "filename-column=i"  => \$filename_column
+my %FILE_TYPE_TO_COLOR = (
+	"di" => "\033[01;34m",
+	"ln" => "\033[01;36m",
+	"ex" => "\033[01;32m",
+	"so" => "\033[01;35m",
+	"pi" => "\033[40;33m",
+	"bd" => "\033[40;33;01m",
+	"cd" => "\033[40;33;01m",
+	"su" => "\033[37;41m",
+	"sg" => "\033[30;43m",
 );
 
+foreach (split(':', $ENV{'TAR_COLORS'} || '')) {
+	my ($type, $codes) = split('=');
+	$FILE_TYPE_TO_COLOR{$type} = "\033[" . $codes . "m";
+}
+
 while (<>) {
-	my @fields = split();
-	my $filename_column = get_filename_column(@fields);
-    my $filename = $fields[$filename_column];
 	my $type = get_file_type();
 
 	if ($type) {
-		color_filename($filename, $FILE_TYPE_TO_COLOR{$type});
+		color_filename($FILE_TYPE_TO_COLOR{$type});
 	}
 
     print;
