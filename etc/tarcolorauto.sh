435e567a52756908b031c7a79eda70d78f47a198
Marc Abramowitz <marc@marc-abramowitz.com>
2012-02-13 00:04:15 -0800

    Support zsh in addition to bash. Reindent.
    Refs GH-10.

--- etc/tarcolorauto.sh
+++ etc/tarcolorauto.sh
@@ -1,14 +1,14 @@
 tarcolorauto() {
-	if [ "$1" == "activate" -o "$1" == "on" ]; then
-		echo "$FUNCNAME turned on. To turn off, do \"$FUNCNAME off\"."
-		tar() {
-			case "$1" in
-				*tvf*)   command tar "$@" | tarcolor ;;
-			    *)       command tar "$@" ;;
-		    esac
-		}
-	elif [ "$1" == "deactivate" -o "$1" == "off" ]; then
-		unset -f tar
-		echo "$FUNCNAME turned off."
-	fi
+    if [ "$1" = "activate" -o "$1" = "on" ]; then
+        echo "tarcolorauto turned on. To turn off, do \"tarcolorauto off\"."
+        tar() {
+            case "$1" in
+                *tvf*)   command tar "$@" | tarcolor ;;
+                *)       command tar "$@" ;;
+            esac
+        }
+    elif [ "$1" = "deactivate" -o "$1" = "off" ]; then
+        unset -f tar
+        echo "tarcolorauto turned off."
+    fi
 }
