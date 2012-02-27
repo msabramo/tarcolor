7946f8c07cbeaee7b9f236d19475e8f459997eca
Marc Abramowitz <marc@marc-abramowitz.com>
2012-02-26 21:16:57 -0800

    Make tarcolorauto tar wrapper check that stdout is a terminal and that terminal supports colors.
    Fixes GH-15.

--- etc/tarcolorauto.sh
+++ etc/tarcolorauto.sh
@@ -2,10 +2,14 @@ tarcolorauto() {
     if [ "$1" = "activate" -o "$1" = "on" ]; then
         echo "tarcolorauto turned on. To turn off, do \"tarcolorauto off\"."
         tar() {
-            case "$1" in
-                *tvf*)   command tar "$@" | tarcolor ;;
-                *)       command tar "$@" ;;
-            esac
+			if [ -t 1 -a $(tput colors) -ge 8 ]; then
+                case "$1" in
+                    *tvf*)   command tar "$@" | tarcolor ;;
+                    *)       command tar "$@" ;;
+                esac
+		    else
+				command tar "$@"
+		    fi
         }
     elif [ "$1" = "deactivate" -o "$1" = "off" ]; then
         unset -f tar
