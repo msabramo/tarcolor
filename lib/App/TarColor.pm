252bf8d7ffe50163064fb1f7e0141fe572460382
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-05 08:21:45 -0700

    Clean up lib/App/TarColor.pm

--- lib/App/TarColor.pm
+++ lib/App/TarColor.pm
@@ -2,14 +2,5 @@ package App::TarColor;
 
 use warnings;
 use strict;
-# use base 'Exporter';
-
-our $VERSION = '0.006';
-# our @EXPORT = qw( get_file_type );
-
-sub foo {
-    print "foo\n";
-    exit(0);
-}
 
 1;
