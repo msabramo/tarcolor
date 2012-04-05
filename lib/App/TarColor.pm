80bab3b08ec00cafef8cf4c4c23218e9d8fcbff1
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-04 22:54:14 -0700

    Add a lib/App/TarColor.pm as suggested by Andreas J. Koenig; an attempt to get CPAN to index App::TarColor

---
+++ lib/App/TarColor.pm
@@ -0,0 +1,15 @@
+package App::TarColor;
+
+use warnings;
+use strict;
+# use base 'Exporter';
+
+our $VERSION = '0.006';
+# our @EXPORT = qw( get_file_type );
+
+sub foo {
+    print "foo\n";
+    exit(0);
+}
+
+1;
